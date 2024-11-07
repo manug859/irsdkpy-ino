23a1238c7150fdda719b791c74445654f2e49acf
manug859 <manuel.agustin.gomez@mi.unc.edu.ar>
2024-11-07 03:57:19 -0300

    Refactorizar la función rpm_map y mejorar la lectura del estado del botón desde Arduino

--- python/ir_main.py
+++ python/ir_main.py
@@ -20,7 +20,7 @@ def update_joystick_button(button_state):
     joystick.set_button(1, button_state)  # Botón 1 del joystick
 
 
-def rpm_map (rpm):
+def rpm_map(rpm):
     rpm_min = 2000
     rpm_max = 12000
 
@@ -34,7 +34,7 @@ def rpm_map (rpm):
 # Loop de ejemplo
 try:
     while True:
-        if ir.is_initialized and ir.is_connected and arduino.in_waiting > 0:
+        if ir.is_initialized and ir.is_connected:
             # Obtener RPM y velocidad del simulador
             speed = ir['Speed']
             rpm = ir['RPM']
@@ -42,10 +42,17 @@ try:
             # Enviar la velocidad y rpm a Arduino
             data = f"{int(rpm_map(rpm))}\n"
             arduino.write(data.encode('utf-8'))
-            button_state = int(arduino.readline().strip())
-            update_joystick_button(button_state == 0)
-            print(f"Estado del botón: {'Presionado' if button_state == 0 else 'Suelto'}")
+            
+            # Leer el estado del botón de Arduino
+            if arduino.in_waiting > 0:  # Verifica si hay datos disponibles desde Arduino
+                button_state = int(arduino.readline().strip())
+                update_joystick_button(button_state == 0)
+                if button_state == 1:
+                    print("Botón presionado")
+            
+            # Imprimir la información de velocidad y RPM
             print(f"Velocidad: {speed} km/h, RPM: {rpm}")
+        
         time.sleep(0.1)  # Esperar 100 ms para la siguiente lectura
 
 except KeyboardInterrupt:
